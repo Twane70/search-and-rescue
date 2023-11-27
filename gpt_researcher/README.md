24fbe4f28f484b27763371ec3f3d0dac7cdc9cab
Twane70 <tonduantoine@gmail.com>
2023-11-28 01:40:02 +0700

    custom

--- gpt_researcher/README.md
+++ gpt_researcher/README.md
@@ -1,31 +1,5 @@
-# 🔎 GPT Researcher
-[![Official Website](https://img.shields.io/badge/Official%20Website-tavily.com-blue?style=for-the-badge&logo=world&logoColor=white)](https://tavily.com)
-[![Discord Follow](https://dcbadge.vercel.app/api/server/2pFkc83fRq?style=for-the-badge)](https://discord.com/invite/2pFkc83fRq)
 
-[![GitHub Repo stars](https://img.shields.io/github/stars/assafelovic/gpt-researcher?style=social)](https://github.com/assafelovic/gpt-researcher)
-[![Twitter Follow](https://img.shields.io/twitter/follow/tavilyai?style=social)](https://twitter.com/tavilyai)
-[![PyPI version](https://badge.fury.io/py/gpt-researcher.svg)](https://badge.fury.io/py/gpt-researcher)
-
-**GPT Researcher is an autonomous agent designed for comprehensive online research on a variety of tasks.** 
-
-The agent can produce detailed, factual and unbiased research reports, with customization options for focusing on relevant resources, outlines, and lessons. Inspired by the recent [Plan-and-Solve](https://arxiv.org/abs/2305.04091) and [RAG](https://arxiv.org/abs/2005.11401) papers, GPT Researcher addresses issues of speed, determinism and reliability, offering a more stable performance and increased speed through parallelized agent work, as opposed to synchronous operations.
-
-**Our mission is to empower individuals and organizations with accurate, unbiased, and factual information by leveraging the power of AI.**
-
-#### PIP Package
-> **Step 0** - Install Python 3.11 or later. [See here](https://www.tutorialsteacher.com/python/install-python) for a step-by-step guide.
-> **Step 1** - install GPT Researcher package [PyPI page](https://pypi.org/project/gpt-researcher/)
-```bash
-$ pip install gpt-researcher
-```
-> **Step 2** - Create .env file with your OpenAI Key and Tavily API key or simply export it
-```bash
-$ export OPENAI_API_KEY={Your OpenAI API Key here}
-```
-```bash
-$ export TAVILY_API_KEY={Your Tavily API Key here}
-```
-> **Step 3** - Start Coding using GPT Researcher in your own code, example:
+> Start Coding using GPT Researcher in your own code, example:
 ```python
 from gpt_researcher import GPTResearcher
 import asyncio
@@ -44,5 +18,3 @@ if __name__ == "__main__":
     print(report)
 
 ```
-
-
--- gpt_researcher/retrievers/google/google.py
+++ gpt_researcher/retrievers/google/google.py
@@ -1,10 +1,7 @@
-# Tavily API Retriever
+# googlesearch-python Retriever
 
 # libraries
-import os
-import requests
-import json
-from tavily import TavilyClient
+from googlesearch import search
 
 
 class GoogleSearch:
@@ -13,44 +10,13 @@ class GoogleSearch:
     """
     def __init__(self, query):
         """
-        Initializes the TavilySearch object
+        Initializes the GoogleSearch object
         Args:
             query:
         """
         self.query = query
-        self.api_key = self.get_api_key() #GOOGLE_API_KEY
-        self.cx_key = self.get_cx_key() #GOOGLE_CX_KEY
-        self.client = TavilyClient(self.api_key)
 
-    def get_api_key(self):
-        """
-        Gets the Tavily API key
-        Returns:
-
-        """
-        # Get the API key
-        try:
-            api_key = os.environ["GOOGLE_API_KEY"]
-        except:
-            raise Exception("Google API key not found. Please set the GOOGLE_API_KEY environment variable. "
-                            "You can get a key at https://developers.google.com/custom-search/v1/overview")
-        return api_key
-
-    def get_cx_key(self):
-        """
-        Gets the Tavily API key
-        Returns:
-
-        """
-        # Get the API key
-        try:
-            api_key = os.environ["GOOGLE_CX_KEY"]
-        except:
-            raise Exception("Google CX key not found. Please set the GOOGLE_CX_KEY environment variable. "
-                            "You can get a key at https://developers.google.com/custom-search/v1/overview")
-        return api_key
-
-    def search(self, max_results=7):
+    def search(self, max_results=7, lang='en'):
         """
         Searches the query
         Returns:
@@ -58,31 +24,16 @@ class GoogleSearch:
         """
         """Useful for general internet search queries using the Google API."""
         print("Searching with query {0}...".format(self.query))
-        url = f"https://www.googleapis.com/customsearch/v1?key={self.api_key}&cx={self.cx_key}&q={self.query}&start=1"
-        resp = requests.get(url)
 
-        if resp is None:
-            return
-        try:
-            search_results = json.loads(resp.text)
-        except Exception:
-            return
-        if search_results is None:
-            return
-
-        results = search_results.get("items", [])
         search_results = []
 
-        # Normalizing results to match the format of the other search APIs
-        for result in results:
-            # skip youtube results
-            if "youtube.com" in result["link"]:
+        for source in search(self.query, lang=lang, num_results=max_results, advanced=True):
+            if "youtube.com" in source.url:
                 continue
-            search_result = {
-                "title": result["title"],
-                "href": result["link"],
-                "body": result["snippet"],
-            }
-            search_results.append(search_result)
+            search_results.append({
+                'title': source.title,
+                'href': source.url,
+                'body': source.description,
+            })
 
         return search_results
